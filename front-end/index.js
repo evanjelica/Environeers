3b0c44e19830da51675a0f460efc1a13c0d7de30
Abby Thomas <69169237+thomasabigail@users.noreply.github.com>
2021-04-04 11:12:35 -0500

    Added button functionality

---
+++ front-end/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+    $("button").click(function(){
+
+      if($("button").text() == "☰"){
+        $("button").text("🞬");
+      }else{
+        $("button").text("☰");
+      }
+      
+      $("li").toggle("slow");
+    });  
+});

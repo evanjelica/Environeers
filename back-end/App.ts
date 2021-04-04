7db5e9cf901ee35b2a304457547e25614aa87d7b
tabreeksomani <53943583+tabreeksomani@users.noreply.github.com>
2021-04-04 13:19:16 -0700

    Server completed

---
+++ back-end/App.ts
@@ -0,0 +1,112 @@
+// import {Environeer} from './Environeer'
+// import Environeer = module("Environeer");
+require('typescript-require');
+
+var restify = require('restify');
+var Environeer = require('./Environeer');
+
+function getAllTasks(req, res, next) {
+
+    res.json(200, {result:  env.getTasks()});
+    return next();
+
+}
+
+function addPoints(req, res, next) {
+    try {
+        let num = env.addTask(req.params.numTasks);
+
+        res.json(200, {result: num});
+        return next();
+    } catch (e) {
+            res.json(400, {error: e});
+            return next();
+    }
+}
+function signup(req, res, next) {
+    try {
+        let results = env.signUp(req.usage, req.password)
+        res.json(200, {result: results});
+        return next()
+    }catch (e) {
+        res.json(400, {error: e})
+        return next();
+    }
+
+}
+
+function removePoints(req, res, next) {
+    try {
+        let points = env.removePoints(req.params.numTasks);
+        res.json(200, {result: points});
+        return next();
+    }catch (e) {
+        res.json(400, {error: e});
+        return next();
+    }
+}
+
+function login(req, res, next) {
+    try {
+        let user = req.body.username;
+        let pass  = req.body.password;
+        let resp = env.login(user, pass);
+        res.json(200, {result: resp});
+        return next();
+
+    } catch (e) {
+        res.json(400, {error: e});
+        return next();
+    }
+
+}
+function getpoints(req, res, next) {
+try {
+    res.json(200, {result: env.getPoints()});
+}catch (e) {
+        res.json(400, {error: e});
+        return next();
+}
+}
+function addtask(req, res, next) {
+res.json(200, env.addTask(req.body));
+return next();
+}
+
+var server = restify.createServer();
+var env = new Environeer.Environeer(3);
+
+server.get('/tasks', getAllTasks); // returns an array
+server.put("/user/:numTasks", addPoints); //info in url returns status 200 for success and 400 for fail
+
+
+server.del("/user/:numTasks", removePoints); //info in url returns status 200 for success and 400 for fail
+
+server.post("/newUser", signup); // info in body, returns status 200 for success and 400 for fail
+
+server.post("existingUser", login); // info in body returns status 200 for success and 400 for fail
+
+
+server.get("/points", getpoints)  //returns number of points
+
+
+server.post("/tasks", addtask) // info in body
+
+
+
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
+// export class App {
+//         initServer(port) {
+//         console.log("App::initServer( " + port + " ) - start");
+//
+//         const server = new Server(port);
+//         server.start().then(function (val) {
+//             console.log("App::initServer() - started: " + val);
+//         }).catch(function (err: Error) {
+//             Log.error("App::initServer() - ERROR: " + err.message);
+//         });
+//     }
+// }
--- back-end/Environeer.ts
+++ back-end/Environeer.ts
@@ -1,66 +1,66 @@
 import {IEnvironeer} from "./IEnvironeer";
+// export module Environeer {
+    export class Environeer implements IEnvironeer {
+        private readonly tasks: string[];
+        private points: number;
+        private username: string;
 
-export class Environeer implements IEnvironeer{
-    private readonly tasks: string[];
-    private points: number;
-    private username: string;
-
-    private readonly pointsPerTask: number;
+        private readonly pointsPerTask: number;
 
 
-    constructor(pointsPerTask: number) {
-        this.pointsPerTask = pointsPerTask;
-        this.tasks = ["planted a tree", "reused a shopping bag", "walked instead of taking a car",
-            "showered in a shorter time"];
-    }
-    // firebase update points
-    addPoints(numberOfTasks: number): number {
-        this.points += numberOfTasks * this.pointsPerTask;
-        //update points
-        return this.points;
-    }
-
-    addTask(task: string): string[] {
-        this.tasks.push(task);
-        return this.tasks;
-    }
-
-    getPoints(): number {
-        return this.points;
-    }
-
-    getTasks(): string[] {
-        return this.tasks;
-    }
-
-    // Need username collection
-    login(username: string, password: string): string {
-        let usernameCollection = {password: "", points: 0};
-        if (password === usernameCollection.password) {
-            this.points = usernameCollection.points;
-            this.username = username;
-            return "Login successful";
+        constructor(pointsPerTask: number) {
+            this.pointsPerTask = pointsPerTask;
+            this.tasks = ["planted a tree", "reused a shopping bag", "walked instead of taking a car",
+                "showered in a shorter time"];
         }
-        else return "Incorrect Username or Password";
-    }
 
-    // Update points in firebase entry
-    removePoints(tasksToRemove: number): number {
-        this.points -= tasksToRemove * this.pointsPerTask;
-        //update points
-        return this.points;
-    }
+        // firebase update points
+        public addPoints(numberOfTasks: number): number {
+            this.points += numberOfTasks * this.pointsPerTask;
+            //update points
+            return this.points;
+        }
 
-    // Need list of all keys from firebase and add username
-    signUp(username: string, password: string): string {
-        let usernames = [] // list of all usernames
-        if (usernames.includes(username)) {
-            throw new Error("Username already exists");
+        public addTask(task: string): string[] {
+            this.tasks.push(task);
+            return this.tasks;
         }
-        else {
-            this.points = 0;
-            // add username entry to database
-            return "Sign up successful";
+
+        public getPoints(): number {
+            return this.points;
+        }
+
+        public getTasks(): string[] {
+            return this.tasks;
+        }
+
+        // Need username collection
+        public login(username: string, password: string): string {
+            let usernameCollection = {password: "", points: 0};
+            if (password === usernameCollection.password) {
+                this.points = usernameCollection.points;
+                this.username = username;
+                return "Login successful";
+            } else throw new Error("Incorrect Username or Password");
+        }
+
+        // Update points in firebase entry
+        public removePoints(tasksToRemove: number): number {
+            this.points -= tasksToRemove * this.pointsPerTask;
+            //update points
+            return this.points;
+        }
+
+        // Need list of all keys from firebase and add username
+        public signUp(username: string, password: string): string {
+            let usernames = [] // list of all usernames
+            if (usernames.includes(username)) {
+                throw new Error("Username already exists");
+            } else {
+                this.points = 0;
+                // add username entry to database
+                return "Sign up successful";
+            }
         }
     }
-}
+// }
